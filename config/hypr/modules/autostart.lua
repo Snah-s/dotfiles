da4b6729d578c5b44ab2711df2eef3b926228853
Snah-s <camiloernestosoto@gmail.com>
2026-09-23 21:07:38 -0500

    ✅ Autostart changed

--- config/hypr/modules/autostart.lua
+++ config/hypr/modules/autostart.lua
@@ -10,7 +10,7 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland")
 	hl.exec_cmd("awww-daemon")
-	hl.exec_cmd("swaync & swayosd-server")
+	hl.exec_cmd("GSK_RENDERER=cairo swaync & GSK_RENDERER=cairo swayosd-server")
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("wl-paste --type text --watch ~/.config/rofi/launchers/clipboard/store-thumb.sh") --- Stores text + renders its thumbnail
